309974e93164ecc04f0a93864a2dbae1fb5c79ef
Stéphane Brunner <courriel@stephane-brunner.ch>
2012-12-29 11:05:52 +0100

    Add mobile version

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -36,15 +36,48 @@ require([
     "jquery-mobile/widgets/page.sections", // for pages (HTML)
     "jquery-mobile/widgets/listview", // HTML
     "jquery-mobile/widgets/controlgroup",
+    "jquery-mobile/widgets/popup",
     "jquery-mobile/widgets/forms/button",
     "jquery-layout",
     "openlayers",
     "mapquery/jquery.mapquery.core"
 ], function($) {
+    $.extend($.mobile, {
+        defaultPageTransition: "none",
+        autoInitializePage: false,
+        linkBindingEnabled: false,
+        hashListeningEnabled: false
+    });
+
     $(function() {
-        $.extend($.mobile, {
-            defaultPageTransition: "none"
-        });
+
+        if ($('body').width() < 800) {
+            $('.small-hide').hide();
+        }
+        else {
+            layout = $('body').layout({
+                'defaults': {
+                    resizable: true,
+                    spacing_open: 4,
+                    spacing_closed: 4
+                },
+                west: {
+                    size: 300,
+                    minSize: 200,
+                    maxSize: 500
+                },
+                east: {
+                    initHidden: true
+                },
+                north: {
+                    size: 130,
+                    resizable: false,
+                    closable: false,
+                    spacing_open: 0
+                }
+            });
+        }
+        $.mobile.initializePage();
 
         var nav = $('<div></div>');
 
@@ -53,7 +86,7 @@ require([
         .button({
         })
         .on('click', function() {
-            $.mobile.changePage("#aboutus");
+            $.mobile.changePage("#aboutus", { changeHash: false });
         });
 
         $('<button>About SV</button>')
@@ -61,7 +94,7 @@ require([
         .button({
         })
         .on('click', function() {
-            $.mobile.changePage('#aboutsv');
+            $.mobile.changePage('#aboutsv', { changeHash: false });
         });
 
         $('<button>Main</button>')
@@ -69,22 +102,25 @@ require([
         .button({
         })
         .on('click', function() {
-            $.mobile.changePage("#main");
+            $.mobile.changePage("#main", { changeHash: false });
         });
 
-        $('<button>Map</button>')
-        .appendTo(nav)
-        .button({
-        })
-        .on('click', function() {
-            $.mobile.changePage("#map2-page");
-            $('#map2').mapQuery({
-                layers: [{
-                    type: 'osm'
-                }],
-                center : {box:[6, 46, 7, 47]}
+        if ($('body').width() < 800) {
+            $('<button>Map</button>')
+            .appendTo(nav)
+            .button({
+            })
+            .on('click', function() {
+                $.mobile.changePage("#map2-page", { changeHash: false });
+/*                $('#map2').mapQuery({
+                    layers: [{
+                        type: 'osm'
+                    }],
+                    theme: null,
+                    center: { position: [6.5, 46.5], zoom: 10 }
+                });*/
             });
-        });
+        }
 
         $('<button>Tost</button>')
         .appendTo(nav)
@@ -102,63 +138,71 @@ require([
             // TODO
         });
 
-        $('<button>One</button>')
-        .appendTo(nav)
-        .button({
-        })
-        .on('click', function() {
-            layout.show('east');
-            $('.ui-layout-east > *').each(function (index, elem) {
-                elem.style.display = 'none';
+
+        if ($('body').width() >= 800) {
+            $('<button>One</button>')
+            .appendTo(nav)
+            .button({
+            })
+            .on('click', function() {
+                layout.show('east');
+                $('.ui-layout-east > *').each(function (index, elem) {
+                    elem.style.display = 'none';
+                });
+                $('.ui-layout-east #one')[0].style.display = 'inline-block';
             });
-            $('.ui-layout-east #one')[0].style.display = 'inline-block';
-        });
 
-        $('<button>Two</button>')
-        .appendTo(nav)
-        .button({
-        })
-        .on('click', function() {
-            layout.show('east');
-            $('.ui-layout-east > *').each(function (index, elem) {
-                elem.style.display = 'none';
+            $('<button>Two</button>')
+            .appendTo(nav)
+            .button({
+            })
+            .on('click', function() {
+                layout.show('east');
+                $('.ui-layout-east > *').each(function (index, elem) {
+                    elem.style.display = 'none';
+                });
+                $('.ui-layout-east #two')[0].style.display = 'inline-block';
             });
-            $('.ui-layout-east #two')[0].style.display = 'inline-block';
-        });
+        }
 
-        nav.appendTo($('#nav'))
-        .controlgroup({
-            type: 'horizontal',
-            mini: true
-        });
+        if ($('body').width() < 800) {
+            var popup = $('<div id="popupMenu">')
+            nav.appendTo(popup)
+            .controlgroup({
+            });
+            popup.appendTo($('body'))
+            .popup();
+            // onpagecreate ...
+            $('<button data-icon="arrow-d" class="ui-btn-right">Menu</button>')
+            .appendTo($('#page > div > [data-role=header]'))
+            .button({
+            })
+            .on('click', function() {
+                $("#popupMenu").popup("open");
+            });
+        }
+        else {
+            nav.appendTo($('#nav'))
+            .controlgroup({
+                type: 'horizontal',
+                mini: true
+            });
+        }
 
-        $('#map').mapQuery({
+        if ($('body').width() < 800) {
+            $.mobile.changePage("#map2-page", { changeHash: false });
+        }
+        else {
+//            $.mobile.changePage("#main", { changeHash: false });
+        }
+                
+        var map = $($('body').width() < 800 ? '#map2' : '#map').mapQuery({
             layers: [{
                 type: 'osm'
             }],
-            center : {box:[6, 46, 7, 47]}
-        });
-
-        layout = $('body').layout({
-            'defaults': {
-                resizable: true,
-                spacing_open: 4,
-                spacing_closed: 4
-            },
-            west: {
-                size: 300,
-                minSize: 200,
-                maxSize: 500
-            },
-            east: {
-                initHidden: true
-            },
-            north: {
-                size: 130,
-                resizable: false,
-                closable: false,
-                spacing_open: 0
-            }
-        });
+            theme: null,
+            center: { position: [6.5, 46.5], zoom: 10 }
+        })
+        .data('mapQuery');
     });
 });
